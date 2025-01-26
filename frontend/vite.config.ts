aa4c3ce9d91d6bfbc7cab2305f40c6757b4fc0c4
Vygintas <vygintas.minkus@gmail.com>
2025-01-26 22:09:06 +0200

    Fixed docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    hmr: {
-      clientPort: 5173,
-    },
-  },
+  base: "http://test4app.codespark.lt/",
 });
